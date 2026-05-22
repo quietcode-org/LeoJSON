40774f949cac149bee9ec66484d0c90e8cb9808d
Andy <andy@quietcode.org>
2026-05-22 21:14:47 +0200

    Add JSONKit smoke probe

---
+++ Makefile
@@ -0,0 +1,37 @@
+SDKROOT ?= /Developer/SDKs/MacOSX10.5.sdk
+ARCH ?= ppc
+MACOSX_VERSION_MIN ?= 10.5
+CC40 ?= /Developer/usr/bin/gcc-4.0
+CC42 ?= /Developer/usr/bin/gcc-4.2
+
+COMMON_FLAGS = -ObjC -std=gnu99 -Wall -DNS_BLOCK_ASSERTIONS=1 \
+	-arch $(ARCH) -isysroot $(SDKROOT) -mmacosx-version-min=$(MACOSX_VERSION_MIN) \
+	-Ivendor/JSONKit
+
+FOUNDATION_FLAGS = -framework Foundation
+
+BUILD_DIR = build
+
+JSONKIT_SRC = vendor/JSONKit/JSONKit.m
+SMOKE_SRC = probes/leojson_jsonkit_smoke.m
+
+.PHONY: all smoke smoke-gcc40 smoke-gcc42 clean
+
+all: smoke
+
+smoke: smoke-gcc40 smoke-gcc42
+
+$(BUILD_DIR):
+	mkdir -p $(BUILD_DIR)
+
+smoke-gcc40: $(BUILD_DIR)
+	$(CC40) $(COMMON_FLAGS) $(SMOKE_SRC) $(JSONKIT_SRC) $(FOUNDATION_FLAGS) \
+		-o $(BUILD_DIR)/leojson_jsonkit_smoke_gcc40
+
+smoke-gcc42: $(BUILD_DIR)
+	$(CC42) $(COMMON_FLAGS) $(SMOKE_SRC) $(JSONKIT_SRC) $(FOUNDATION_FLAGS) \
+		-o $(BUILD_DIR)/leojson_jsonkit_smoke_gcc42
+
+clean:
+	rm -rf $(BUILD_DIR)/leojson_jsonkit_smoke_gcc40
+	rm -rf $(BUILD_DIR)/leojson_jsonkit_smoke_gcc42
--- probes/leojson_jsonkit_smoke.m
+++ probes/leojson_jsonkit_smoke.m
@@ -1,1 +1,63 @@
+#import <Foundation/Foundation.h>
+#import "JSONKit.h"
 
+static int fail(NSString *message)
+{
+    fprintf(stderr, "FAIL: %s\n", [message UTF8String]);
+    return 1;
+}
+
+int main(int argc, const char *argv[])
+{
+    NSAutoreleasePool *pool = [[NSAutoreleasePool alloc] init];
+
+    NSString *jsonString =
+        @"{"
+         "\"name\":\"LeoJSON\","
+         "\"target\":\"Mac OS X 10.5.8 PowerPC\","
+         "\"values\":[1,2,3],"
+         "\"unicode\":\"Grüße vom G5\""
+        "}";
+
+    NSData *jsonData = [jsonString dataUsingEncoding:NSUTF8StringEncoding];
+
+    NSError *error = nil;
+    id object = [jsonData objectFromJSONDataWithParseOptions:JKParseOptionStrict
+                                                       error:&error];
+
+    if (object == nil) {
+        NSString *message = [NSString stringWithFormat:@"parse error: %@", error];
+        [pool drain];
+        return fail(message);
+    }
+
+    if (![object isKindOfClass:[NSDictionary class]]) {
+        [pool drain];
+        return fail(@"parsed root object is not an NSDictionary");
+    }
+
+    NSDictionary *dict = (NSDictionary *)object;
+
+    if (![[dict objectForKey:@"name"] isEqualToString:@"LeoJSON"]) {
+        [pool drain];
+        return fail(@"unexpected name field");
+    }
+
+    NSData *roundtrip = [dict JSONData];
+
+    if (roundtrip == nil || [roundtrip length] == 0) {
+        [pool drain];
+        return fail(@"serialization returned empty data");
+    }
+
+    NSString *roundtripString =
+        [[NSString alloc] initWithData:roundtrip encoding:NSUTF8StringEncoding];
+
+    printf("OK: JSONKit smoke probe passed\n");
+    printf("Roundtrip JSON: %s\n", [roundtripString UTF8String]);
+
+    [roundtripString release];
+    [pool drain];
+
+    return 0;
+}
